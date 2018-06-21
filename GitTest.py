1860ee5195277c89e33070f7a8ac672a5f6a971f
Fanatic-Coder <GXenakis2@gmail.com>
2018-06-21 14:23:04 +0300

    Just a Git Test subtracting 2 numbers

--- GitTest.py
+++ GitTest.py
@@ -1,5 +1,5 @@
-print("Hello Dudes and Girls! I'm Just Testing Github and Git!")
-print("I'm gonna subtract 2 numbers! Impressive Huh? xD")
+print("Hello Guys and Girls! I'm Just Testing Github and Git!")
+print("I'm gonna subtract 2 numbers!")
 
 a = int(input("Give me your first number: "))
 b = int(input("Give me your second number: "))
